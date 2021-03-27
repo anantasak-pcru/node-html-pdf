f8bb3d26826e7eacd641463cd3960a9cc4d6765e
Anantasak <anantasak.non@pcru.ac.th>
2021-03-27 14:59:16 +0700

    Fix require all value

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,30 @@
 declare module "node-html-pdf"
 
 interface Options {
-    displayHeaderFooter: boolean
-    headerTemplate: string
-    footerTemplate: string
-    printBackground: boolean
-    landscape: boolean
-    pageRanges: string
-    format: string | 'Letter' | 'Legal' | 'Tabloid' | 'Ledger' | 'A0' | 'A1' | 'A2' | 'A3' | 'A4' | 'A5' | 'A6'
-    width: string | number
-    height: string | number
-    margin: {
-        top: string | number
-        right: string | number
-        bottom: string | number
-        left: string | number
+    displayHeaderFooter?: boolean
+    headerTemplate?: string
+    footerTemplate?: string
+    printBackground?: boolean
+    landscape?: boolean
+    pageRanges?: string
+    format?: string | 'Letter' | 'Legal' | 'Tabloid' | 'Ledger' | 'A0' | 'A1' | 'A2' | 'A3' | 'A4' | 'A5' | 'A6'
+    width?: string | number
+    height?: string | number
+    margin?: {
+        top?: string | number
+        right?: string | number
+        bottom?: string | number
+        left?: string | number
     }
-    preferCSSPageSize: boolean
+    preferCSSPageSize?: boolean
 }
 
 interface PdfOptions {
-    options: Options
-    html: string
-    path: string
-    type: 'pdf' | 'buffer'
-    data: Object
+    options?: Options
+    html?: string
+    path?: string
+    type?: 'pdf' | 'buffer'
+    data?: Object
 }
 
 export function create(options: PdfOptions): Promise<Buffer | undefined>
